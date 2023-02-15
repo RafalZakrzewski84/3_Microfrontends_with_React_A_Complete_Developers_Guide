e853fab6707dc7ec5b6207bf86d33bacdb893756
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-02-15 14:02:56 +0100

    configure webPack and running app in browser

--- 1_Basics_of_Microfrondends/ecomm/products/webpack.config.js
+++ 1_Basics_of_Microfrondends/ecomm/products/webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
+  devServer: {
+    port: 8081,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
