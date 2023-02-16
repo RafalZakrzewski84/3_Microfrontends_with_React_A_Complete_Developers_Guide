a4e300df0fc6142e33bb57e10fe5de84f07020bb
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-02-16 23:04:25 +0100

    3 sharing dependencies - packages (modules) sharing

--- 1_Basics_of_Microfrondends/ecomm/cart/src/index.js
+++ 1_Basics_of_Microfrondends/ecomm/cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const cartMsg = `<div>You have ${faker.random.number()} items in your cart</div>`;
-
-document.getElementById('dev-cart').innerHTML = cartMsg;
+import('./bootstrap');
--- 1_Basics_of_Microfrondends/ecomm/products/src/index.js
+++ 1_Basics_of_Microfrondends/ecomm/products/src/index.js
@@ -1,13 +1,1 @@
-import faker from 'faker';
-
-console.log('From products');
-
-let products = '';
-
-for (let i = 0; i < 10; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
-}
-
-const devProducts = document.getElementById('dev-products');
-devProducts.innerHTML = products;
+import('./bootstrap');
