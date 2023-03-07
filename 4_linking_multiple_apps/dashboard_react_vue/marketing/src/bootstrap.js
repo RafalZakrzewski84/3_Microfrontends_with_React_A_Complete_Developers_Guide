13a06ed57ae059422d5fe4090c00b1a7ae1c9144
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-03-07 16:41:44 +0100

    10_Multi-tier_Navigation - running Marketing memory history in isolation

--- 4_linking_multiple_apps/dashboard_react_vue/marketing/src/bootstrap.js
+++ 4_linking_multiple_apps/dashboard_react_vue/marketing/src/bootstrap.js
@@ -6,7 +6,11 @@ import App from './App';
 //Mount function to start up the app
 const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
-  history.listen(onNavigate);
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(<App history={history} />, el);
 };
 
@@ -15,7 +19,7 @@ const mount = (el, { onNavigate }) => {
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
