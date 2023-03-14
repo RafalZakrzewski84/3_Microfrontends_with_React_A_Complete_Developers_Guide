59fc38f2ca806d0df9989fd4094948e8e184c01d
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-03-14 15:21:50 +0100

    11_Performance - adding lading bar, auth prod deploy, link auth in prod

--- 4_linking_multiple_apps/dashboard_react_vue/container/src/App.js
+++ 4_linking_multiple_apps/dashboard_react_vue/container/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
+import Progress from './components/Progress';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
   return (
@@ -18,10 +21,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
